859d00e3f198f8644fe107091d7a0f960b999354
bally <device_overflow@hotmail.com>
2012-08-25 21:39:29 +0700

    add ui project

---
+++ source/ui/iHospital.Ui/iHospital.Ui/iHospital_UiFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  iHospital_UiFirstViewController.h
+//  iHospital.Ui
+//
+//  Created by  thanan bovornsaksakul on 8/25/55 BE.
+//  Copyright (c) 2555 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iHospital_UiFirstViewController : UIViewController
+
+@end
